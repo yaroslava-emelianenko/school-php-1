0e9d4cb3036324694676630456825d7766f5d824
yaroslava-emelianenko <yarhik.69@gmail.com>
2021-11-02 19:42:37 +0200

    Домашняя работа 10

---
+++ 10/10/index.php
@@ -0,0 +1,30 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'on');
+header('Content-Type: text/html; charset=utf-8');
+
+/*
+ * CONTROLLER BLOCK + MODEL BLOCK
+ * В этой части идёт создание и обработка PHP кода
+ */
+$title = 'Домашняя работа php 10';
+
+$haystack = 'one two three';
+$needle = 'two';
+$result = strpos($haystack, $needle);
+var_dump($result);
+
+/*
+ * VIEW BLOCK / Вид сайта
+ * В этой части идёт вывод HTML и PHP
+ */
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+
+</head>
+<body>
+</body>
